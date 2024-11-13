c432b0d3c604aae251f9cc221d05777c4bd4330b
angelazhou32 <angela.zhou@temporal.io>
2024-11-13 12:19:15 -0500

    resolve merge

--- samples/using-records/Worker/Program.cs
+++ samples/using-records/Worker/Program.cs
@@ -1,24 +1,41 @@
-// // This file is designated to run the Worker
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
+// This file is designated to run the Worker
+using System.Diagnostics;
 using Microsoft.Extensions.Logging;
 using Temporalio.Client;
-using Temporalio.Extensions.Hosting;
 using Temporalio.Worker;
 using TemporalioDurableExecution;
 
-var host = Host.CreateDefaultBuilder(args)
-    .ConfigureLogging(ctx => ctx.AddSimpleConsole().SetMinimumLevel(LogLevel.Information))
-    .ConfigureServices(ctx =>
-        ctx.
-            // Add the worker
-            AddHostedTemporalWorker(
-                clientTargetHost: "localhost:7233",
-                clientNamespace: "default",
-                taskQueue: WorkflowConstants.TaskQueueName).
-            // Add the activities class at the scoped level
-            AddScopedActivities<DurableExecutionActivities>().
-            AddWorkflow<TranslationWorkflow>())
-    .Build();
+// Create a client to localhost on "default" namespace
+var client = await TemporalClient.ConnectAsync(new("localhost:7233")
+{
+    LoggerFactory = LoggerFactory.Create(builder =>
+        builder.AddSimpleConsole(options => options.TimestampFormat = "[HH:mm:ss] ").SetMinimumLevel(LogLevel.Information)),
+});
 
-await host.RunAsync();
+// Cancellation token to shutdown worker on ctrl+c
+using var tokenSource = new CancellationTokenSource();
+Console.CancelKeyPress += (_, eventArgs) =>
+{
+    tokenSource.Cancel();
+    eventArgs.Cancel = true;
+};
+
+var activities = new DurableExecutionActivities();
+
+// Create worker
+using var worker = new TemporalWorker(
+    client,
+    new TemporalWorkerOptions(WorkflowConstants.TaskQueueName)
+        .AddAllActivities(activities)
+        .AddWorkflow<TranslationWorkflow>());
+
+// Run worker until cancelled
+Console.WriteLine("Running worker");
+try
+{
+    await worker.ExecuteAsync(tokenSource.Token);
+}
+catch (OperationCanceledException)
+{
+    Console.WriteLine("Worker cancelled");
+}
